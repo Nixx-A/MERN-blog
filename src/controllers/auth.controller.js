a2422007da93f4c94fd4fabda366e76e361224df
Nicolas Ayan <nicolas.ayan18'@'gmail.com>
2023-08-20 13:40:26 -0300

    Add model auth and post

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,69 +1,44 @@
 import User from '../models/database/User.js'
-import bcrypt from 'bcryptjs'
-import { createAccessToken } from '../libs/jtw.js'
 import jwt from 'jsonwebtoken'
 import { TOKEN_SECRET } from '../config.js'
+import { AuthModel } from "../models/local-file-system/auth.js";
 
 export class AuthController {
-  static async register(req, res) {
+  static async register (req, res) {
     const { username, email, password } = req.body
     try {
       const userFound = await User.findOne({ email })
-      if (userFound)
-        return res.status(400).json({ message: 'User already exists' })
-
-      const hashedPassword = await bcrypt.hash(password, 10)
-      const newUser = new User({ username, email, password: hashedPassword })
-
-      const userSaved = await newUser.save()
-      const token = await createAccessToken({ id: userSaved._id })
+      if (userFound) return res.status(400).json({ message: 'User already exists' })
+      
+      const { user, token } = await AuthModel.register({ username, email, password, res })
       res.cookie('token', token)
-
-      res.json({
-        id: userSaved._id,
-        username: userSaved.username,
-        email: userSaved.email,
-        data_registered: userSaved.data_registered
-      })
+      res.json(user)
     } catch (err) {
       res.status(500).json({ error: err.message })
     }
   }
 
-  static async login(req, res) {
+  static async login (req, res) {
     const { email, password } = req.body
-
     try {
-      const userFound = await User.findOne({ email })
-      if (!userFound) return res.status(400).json({ message: 'User not found' })
+      const { user, token, isMatch } = await AuthModel.login({ email, password })
+      if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' })
 
-      const isMatch = await bcrypt.compare(password, userFound.password)
-      if (!isMatch)
-        return res.status(400).json({ message: 'Invalid credentials' })
-
-      const token = await createAccessToken({ id: userFound._id })
       res.cookie('token', token)
-
-      res.json({
-        id: userFound._id,
-        username: userFound.username,
-        email: userFound.email,
-        data_registered: userFound.data_registered
-      })
+      res.json(user)
     } catch (error) {
       res.status(500).json({ error: error.message })
     }
   }
 
-  static async logout(req, res) {
+  static async logout (req, res) {
     res.clearCookie('token')
     res.json({ message: 'Logged out' })
   }
 
-  static async verifyToken(req, res) {
+  static async verifyToken (req, res) {
     const { token } = req.cookies
     if (!token) return res.status(401).json({ message: 'Unauthorized' })
-    console.log(token)
 
     jwt.verify(token, TOKEN_SECRET, async (err, decoded) => {
       if (err) return res.status(401).json({ message: 'Unauthorized' })
@@ -79,7 +54,7 @@ export class AuthController {
     })
   }
 
-  static async profile(req, res) {
+  static async profile (req, res) {
     res.json({ message: 'Is working well' })
   }
 }
---
+++ src/models/local-file-system/auth.js
@@ -0,0 +1,38 @@
+import bcrypt from 'bcryptjs'
+import User from '../database/User.js'
+import { createAccessToken } from '../../libs/jtw.js'
+
+export class AuthModel {
+  static async register ({ username, email, password }) {
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = new User({ username, email, password: hashedPassword })
+
+    const userSaved = await newUser.save()
+    const token = await createAccessToken({ id: userSaved._id })
+
+    const user = {
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      data_registered: userSaved.data_registered
+    }
+
+    return { user, token }
+  }
+
+  static async login ({ email, password }) {
+    const userFound = await User.findOne({ email })
+    if (!userFound) return res.status(400).json({ message: 'User not found' })
+
+    const isMatch = await bcrypt.compare(password, userFound.password)
+    const token = await createAccessToken({ id: userFound._id })
+
+    const user = {
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      data_registered: userFound.data_registered
+    }
+    return { user, token, isMatch }
+  }
+}

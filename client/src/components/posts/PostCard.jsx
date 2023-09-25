c5b6399886805f22af5f392acbb27a0580db3e83
Nicolas Ayan <nicolas.ayan18'@'gmail.com>
2023-09-25 18:35:22 -0300

    Add new features to ProfilePage Finish SettingsPage

--- client/src/components/posts/PostCard.jsx
+++ client/src/components/posts/PostCard.jsx
@@ -10,46 +10,45 @@ export default function PostCard ({ post }) {
 
   return (
     <div className='bg-white mb-4 rounded p-4 w-[95%]'>
-      <Link to={`/post/${post._id}`}>
-        <div className='flex items-center'>
-          <img
-            className='w-10 h-10 rounded-full mr-2 '
-            src='/not-user.jpg'
-          />
-          <div className=''>
-            <Link
-              className='font-semibold text-gray-800 hover:text-black'
-              to={`/user/${post.author.username}`}>
-              {post.author.username}
-            </Link>
-            <p className='font-light'>{formattedDate}</p>
-          </div>
+      <div className='flex items-center'>
+        <img
+          className='w-10 h-10 rounded-full mr-2 '
+          src='/not-user.jpg'
+        />
+        <div className=''>
+          <Link
+            className='font-semibold text-gray-800 hover:text-black'
+            to={`/${post.author._id}`}>
+            {post.author.username}
+          </Link>
+          <p className='font-light'>{formattedDate}</p>
         </div>
+      </div>
 
-        <div className='text-xl font-semibold mb-2 inline-block hover:text-indigo-700'>{post.title}</div>
+      <h2 className='text-xl font-semibold mb-2 inline-block hover:text-indigo-700 cursor-pointer'><Link to={`/post/${post._id}`}>{post.title}</Link></h2>
 
+      {
+        post.tags.name &&
         <div>
-          <p>
-            {post.tags.map(tag => (
-              <Link
-                className='hover:text-black text-gray-800  hover:bg-gray-100  px-1.5 py-0.5 hover:border-gray-300 border duration-150 border-transparent rounded'
-                to={`/tag/${tag._id}/${tag.name}`}
-                key={tag.name}>
-                #{tag.name}
-              </Link>
-            ))}
-          </p>
+          {post.tags.map(tag => (
+            <Link
+              className='hover:text-black text-gray-800  hover:bg-gray-100  px-1.5 py-0.5 hover:border-gray-300 border duration-150 border-transparent rounded'
+              to={`/tag/${tag._id}/${tag.name}`}
+              key={tag.name}>
+              #{tag.name}
+            </Link>
+          ))}
         </div>
+      }
 
-        <div className='flex items-center w-[95%] m-auto  '>
-          <AiOutlineLike size={20} className='mr-2' color='gray ' />
-          <div className='flex items-center hover:bg-gray-100 gap-x-1 px-1.5 py-0.5 border-none hover:border rounded'>
-            <FaRegCommentDots color='gray' />
-            <p>{post.comments.length}</p>
-          </div>
-          <small className='ml-auto'>{readingTime} min read</small>
+      <div className='flex items-center w-[95%] m-auto  '>
+        <AiOutlineLike size={20} className='mr-2' color='gray ' />
+        <div className='flex items-center hover:bg-gray-100 gap-x-1 px-1.5 py-0.5 border-none hover:border rounded'>
+          <FaRegCommentDots color='gray' />
+          <p>{post.comments.length}</p>
         </div>
-      </Link>
-    </div>
+        <small className='ml-auto'>{readingTime} min read</small>
+      </div>
+    </div >
   )
 }
